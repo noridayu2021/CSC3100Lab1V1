37f7b24037bdf7a75b16677fba578bcf7b06a334
Muniif A. <muniif10@gmail.com>
2021-10-20 12:17:31 +0800

    Added Lab 1 Questions

---
+++ CSC3100Lab1V1/src/MakingMoneyATM.java
@@ -0,0 +1,20 @@
+//import java.util.*;
+
+public class MakingMoneyATM {
+	public static void main(String[]args) {
+		/* System.out.print("Enter your name: "); 
+		Scanner input = new Scanner(System.in); // Object scanner named input
+		String userName = input.next(); // 
+		input.close(); */
+		
+		System.out.println("     ______________________________________________________________");
+		System.out.println("    /                                                             /");
+		System.out.println("   /                                                             / ");
+		System.out.println("  /                  Making Money Bank ATM Service              /  ");
+		System.out.println(" /                                                             /   ");
+		System.out.println("/_____________________________________________________________/    ");
+		System.out.println("\nWelcome! ");
+		
+		//System.out.println("\nWelcome! " + userName);
+	}
+}
