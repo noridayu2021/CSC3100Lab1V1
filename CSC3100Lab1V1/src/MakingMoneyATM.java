912b3e4568ce7fb2a77d25d08e103e22e7c88dd9
HP <HP@gajahkentutt>
2021-10-20 20:06:54 +0800

    Lab Question 4

---
+++ CSC3100Lab1V1/src/MakingMoneyATM.java
@@ -0,0 +1,22 @@
+
+public class MakingMoneyATM {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Lab Session 1");
+		System.out.println("Lab Question 4");
+		System.out.println("  ");
+		
+		System.out.println(" ======================================================================" );
+		System.out.println("|                                                                      |");
+		System.out.println("|                   Making Money Bank ATM Service                      |");
+		System.out.println("|                                                                      |");
+		System.out.println(" ====================================================================== ");
+		
+		System.out.println("  ");
+		System.out.println("Welcome dear user !");
+		System.out.println("Making money with us in easier and faster way");
+		System.out.println("Have a nice <3");
+	}
+
+}
