4d1f11f673346ca821d6ceeb53ed72b789f1ec1d
ultim <ultim@DESKTOP-33E1R3S>
2021-10-20 21:53:43 +0800

    Student

---
+++ CSC3100Lab1V1/src/Student.java
@@ -0,0 +1,12 @@
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sahtish Rao Ramuloo");
+		System.out.println("210404");
+		System.out.println("Kolej 12");
+
+	}
+
+}
