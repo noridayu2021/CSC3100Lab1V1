2d62f104dd210a2fb8c2f28d2ddf0008d5e3b43c
Izza Syahirah <Izza Syahirah@LAPTOP-H0OBH6OE.realtek>
2021-10-20 11:38:22 +0800

    added two lines

--- CSC3100Lab1V1/src/HelloClass.java
+++ CSC3100Lab1V1/src/HelloClass.java
@@ -1,11 +1,13 @@
 
 public class HelloClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Lab Session 1");
 		System.out.println("Lab Session");
-
+		System.out.println("Hello everyone");
+		System.out.println("Have a nice day");
 	}
 
 }
