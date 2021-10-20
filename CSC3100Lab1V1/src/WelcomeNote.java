f7416d037bf90df6d7b1080b07f150dc41b5e2fb
User <User@ArifDanial>
2021-10-20 16:07:52 +0800

    This is my Welcome Note Java

---
+++ CSC3100Lab1V1/src/WelcomeNote.java
@@ -0,0 +1,9 @@
+
+public class WelcomeNote {
+	public static void main (String[]args) {
+		System.out.println("Welcome");
+		System.out.println("");
+		System.out.println("Java");
+		System.out.println("Programming !");
+}
+}
