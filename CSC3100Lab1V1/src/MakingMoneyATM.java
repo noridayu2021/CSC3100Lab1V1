1e9b1898306a48f4630014bfd8c688e3cb30ac8e
DANESHWAREN <DANESHWAREN@LAPTOP-GL5OHIF4>
2021-10-20 22:46:40 +0800

    LAB 1 question 4

---
+++ CSC3100Lab1V1/src/MakingMoneyATM.java
@@ -0,0 +1,12 @@
+
+public class MakingMoneyATM {
+	public static void main(String args[]) {
+		System.out.println("*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*");
+		System.out.println("*                   Welcome to                          *");
+		System.out.println("*          Making Money Bank ATM Service!!!             *");
+		System.out.println("*                 (0)        (0)                        *");
+		System.out.println("*              |____________________|                   *");
+		System.out.println("*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*|*");
+	}
+
+}
