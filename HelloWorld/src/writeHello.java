98ae35d78324f91276f89f7a5bbdbbeb042261a8
USER <USER@LAPTOP-G1P18T2M>
2021-10-20 11:46:07 +0800

    second commit

--- HelloWorld/src/writeHello.java
+++ HelloWorld/src/writeHello.java
@@ -3,7 +3,8 @@ public class writeHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello");
+      System.out.println("Hello");
+      System.out.println("Second commit");
 	}
 
 }
