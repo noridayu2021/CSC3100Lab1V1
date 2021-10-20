b3a8dd3a2d79465740bbbee70e15b293b971d062
Danish <Danish@PavGamingDanish>
2021-10-20 10:58:12 +0800

    added hi

--- CSC3100Lab1V1/src/HelloClass.java
+++ CSC3100Lab1V1/src/HelloClass.java
@@ -5,7 +5,8 @@ public class HelloClass {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Lab Session 1");
 		System.out.println("Lab Session");
-
+System.out.println("Hi everyone!");
+System.out.println("Hi again.");
 	}
 
 }
