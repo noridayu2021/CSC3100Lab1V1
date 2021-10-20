3223ce7b1e810c02abf7c7c25873a7b618495915
User <User@ArifDanial>
2021-10-20 15:54:56 +0800

    This is exercise 4

---
+++ CSC3100Lab1V1/src/MakingMoneyATM.java
@@ -0,0 +1,18 @@
+
+public class MakingMoneyATM {
+	public static void main(String[] args) {
+		System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("$                                                              $");
+		System.out.println("$                 Making Money Bank ATM Service                $");
+		System.out.println("$                                                              $");
+		System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("");
+		System.out.println("WELCOME !");
+		System.out.println("");
+		System.out.println("Account No : 98668747858 ");
+		System.out.println("Account User Name: Arif Danial");
+		System.out.println("Account Balance: RM 64.00 ");
+		System.out.println("Amount Money To Withdraw: RM 50.00");
+		System.out.println("Amound To Deposit Fund: ");
+	}
+}
