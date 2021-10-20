e12e81860fded603dba275d479c08e31b548aceb
noridayumanshor <noridayumanshor@10.206.2.148>
2021-10-20 10:22:24 +0800

    Added Lab Session Message

--- CSC3100Lab1V1/src/HelloClass.java
+++ CSC3100Lab1V1/src/HelloClass.java
@@ -4,6 +4,7 @@ public class HelloClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Lab Session 1");
+		System.out.println("Lab Session");
 
 	}
 
