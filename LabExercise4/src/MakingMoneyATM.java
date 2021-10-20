69be407505190741b6a1c252889740c92fe68cbc
USER <USER@LAPTOP-G1P18T2M>
2021-10-20 12:29:28 +0800

    Lab Exercise 4 (by Chew)

---
+++ LabExercise4/src/MakingMoneyATM.java
@@ -0,0 +1,12 @@
+
+public class MakingMoneyATM {
+ public static void main (String[]args)
+ {
+	 System.out.println("****************************************");
+	 System.out.println("*\t\t\t\t\t*");
+	 System.out.println("\n*    Making Money Bank ATM Services\t*");
+	 System.out.println("\n*\t\t\t\t\t*");
+	 System.out.println("****************************************");
+	 System.out.println("\nWelcome!");
+ }
+}
