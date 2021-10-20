8a88ff2601fe5e033b3d18556d455b6ef54f81a6
ultim <ultim@DESKTOP-33E1R3S>
2021-10-20 21:58:10 +0800

    Multiple Line

---
+++ CSC3100Lab1V1/src/MultipleLine.java
@@ -0,0 +1,13 @@
+
+public class MultipleLine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+		System.out.println("");
+		System.out.println("Java");
+		System.out.println("Programming!");
+
+	}
+
+}
