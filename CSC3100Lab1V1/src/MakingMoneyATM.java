cb2b07de5c709018543c53364752423c3d9ec94d
ultim <ultim@DESKTOP-33E1R3S>
2021-10-20 22:12:02 +0800

    Making Money ATM

---
+++ CSC3100Lab1V1/src/MakingMoneyATM.java
@@ -0,0 +1,16 @@
+
+public class MakingMoneyATM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("........................................................");
+		System.out.println(".                                                      .");
+		System.out.println(".            Making Money Bank ATM Service             .");
+		System.out.println(".                    *\\_($_$)_/*                       .");
+		System.out.println(".                      Welcome                         .");
+		System.out.println("........................................................");
+		
+
+	}
+
+}
