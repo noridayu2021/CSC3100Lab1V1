6638b74fb3c8698aebad0f412eea0932fc41f2d6
DANESHWAREN <DANESHWAREN@LAPTOP-GL5OHIF4>
2021-10-20 15:39:03 +0800

    added my name and a welcome

--- CSC3100Lab1V1/src/HelloClass.java
+++ CSC3100Lab1V1/src/HelloClass.java
@@ -5,6 +5,9 @@ public class HelloClass {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Lab Session 1");
 		System.out.println("Lab Session");
+		System.out.println("Welcome guys");
+		System.out.println("helllllooooo");
+		System.out.println("my name is Danesh");
 
 	}
 
