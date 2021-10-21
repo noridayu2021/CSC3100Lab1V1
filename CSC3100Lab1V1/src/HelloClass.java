803c4ffb1b76c6232450d6e527d78993cfe543a2
H.P <H.P@User.realtek>
2021-10-22 00:27:59 +0800

    comitting

--- CSC3100Lab1V1/src/HelloClass.java
+++ CSC3100Lab1V1/src/HelloClass.java
@@ -5,7 +5,9 @@ public class HelloClass {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Lab Session 1");
 		System.out.println("Lab Session");
-
+		System.out.println("heyyooo");
+		System.out.println("alya here");
+		System.out.println("testing");
 	}
 
 }
