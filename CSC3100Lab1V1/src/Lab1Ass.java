cf0b844f13b25647e5a28206b6a060450dab836f
dayselew <dayselew@localhost>
2021-10-21 15:50:53 +0800

    Dayse's Lab 1 Ass

---
+++ CSC3100Lab1V1/src/Lab1Ass.java
@@ -0,0 +1,15 @@
+
+public class Lab1Ass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("***********************************************"); 
+		System.out.println("*                                             *");
+		System.out.println("*        Making money Bank ATM Service        *");
+		System.out.println("*                   Welcome!                  *");
+		System.out.println("*                                             *");
+		System.out.println("***********************************************"); 
+
+		}
+
+}
