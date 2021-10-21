23e175c633786eb2fbac4dc233a3e6d721370a22
noridayumanshor <noridayumanshor@noridayus-mbp.lan>
2021-10-21 20:26:42 +0800

    Commit Selamat Malam

---
+++ CSC3100Lab1V1/src/GoodNight.java
@@ -0,0 +1,11 @@
+
+public class GoodNight {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Selamat Malam");
+		System.out.println("Noridayu");
+
+	}
+
+}
