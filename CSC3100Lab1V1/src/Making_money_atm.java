1799be483491217dfc2fb3af0227805dde852798
User <User@DESKTOP-R7EFO8P>
2021-10-21 12:23:16 +0800

    commit Q4

---
+++ CSC3100Lab1V1/src/Making_money_atm.java
@@ -0,0 +1,22 @@
+
+public class Making_money_atm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("$                                                              $");
+		System.out.println("$                 Making Money Bank ATM Service                $");
+		System.out.println("$                                                              $");
+		System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+		System.out.println("");
+		System.out.println("WELCOME !");
+		System.out.println("");
+		System.out.println("Account No : 2141414141 ");
+		System.out.println("Account User Name: Muadz");
+		System.out.println("Account Balance: RM 9000.00 ");
+		System.out.println("Amount Money To Withdraw: RM 500.00");
+		System.out.println("Amound To Deposit Fund: ");
+
+	}
+
+}
