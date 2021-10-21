3c331e6cc66b6ca4efba06306ab684ea4e94f1a3
Bavamithiran <Bavamithiran@DESKTOP-VDJ7QPB>
2021-10-21 14:56:24 +0800

    Making Money Bank ATM Service$$$$$$

--- CSC3100Lab1V1/src/MakingMoneyBank.java
+++ CSC3100Lab1V1/src/MakingMoneyBank.java
@@ -2,12 +2,12 @@
 public class MakingMoneyBank {
 
 	public static void main(String[] args) {
-		System.out.println("........................................................");
-		System.out.println(".                                                      .");
-		System.out.println(".            Making Money Bank ATM Service             .");
-		System.out.println(".                    *\\_($_$)_/*                       .");
-		System.out.println(".                      Welcome                         .");
-		System.out.println("........................................................");
+		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("!                     WELCOME TO                       !");
+		System.out.println("!            $Making Money Bank ATM Service$           !");
+		System.out.println("!                    $$$$$$$$$$$$                      !");
+		System.out.println("!                                                      !");
+		System.out.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
 		// TODO Auto-generated method stub
 
 	}
