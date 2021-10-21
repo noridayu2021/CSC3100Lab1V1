f6e8dd0e49264938ae393de61b0c3cda1982ef73
Alvin RV XII-MIPA 6 <Alvin RV XII-MIPA 6@DESKTOP-I19K517>
2021-10-21 15:04:28 +0700

    REVISION

--- CSC3100Lab1V1/src/AlvinBank.java
+++ CSC3100Lab1V1/src/AlvinBank.java
@@ -2,7 +2,28 @@
 public class AlvinBank {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("================================================================");
+		System.out.println("|															   |");
+		System.out.println("| 				Welcome to ARV ATM Center					   |");
+		System.out.println("|															   |");
+		System.out.println("================================================================");
+		System.out.println("DATE 	 TIME	TERMINAL										");
+		System.out.println("15/7 	 12.00	ABCDEFG2										");
+		System.out.println(" 																");
+		System.out.println("BALACE   $ 2.000.000											");
+		System.out.println("															    ");
+		System.out.println("TRANSFER $ 900 													");
+		System.out.println("RECEIVER  														");
+		System.out.println("NAME   :  MICHAEL RHAPSODY										");
+		System.out.println("REK.NO :  84849490342929										");
+		System.out.println("Status Complete													");
+		System.out.println("                                                                ");
+		System.out.println(" 				THANK YOU FOR USING OUR SERVICE					");
+		System.out.println("----------------------------------------------------------------");
+
+	}
+
+}
 
 	}
 
