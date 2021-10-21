12880f1d491e5bd48eee3ce00c10b00cc9bc9f3f
hopelessScrewdriver <92832451+hopelessScrewdriver@users.noreply.github.com>
2021-10-21 20:00:46 +0800

    Delete Lab1 directory

--- Lab1/src/PrintWelcome.java
+++
@@ -1,6 +0,0 @@
-public class PrintWelcome {
- public static void main(String[]args)
- {
-	 System.out.print("Welcome\n\nJava\nProgramming");
- }
-}
--- Lab1/src/Student.java
+++
@@ -1,8 +0,0 @@
-
-public class Student {
- public static void main (String[]args)
- {
-	 System.out.print("Chew Zi Qing\n212360\nKuala Lumpur");
- }
-}
-
