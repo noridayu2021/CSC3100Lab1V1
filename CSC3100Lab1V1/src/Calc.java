9e724bdf414e9e207a04d519797db1f1acfa0d7d
USER <USER@LAPTOP-G1P18T2M>
2021-10-21 17:33:16 +0800

    extra exercises for lab 1

--- CSC3100Lab1V1/src/Calc.java
+++ CSC3100Lab1V1/src/Calc.java
@@ -3,6 +3,6 @@ public class Calc {
  public static void main(String[]args)
  {
 	 System.out.print((7.5*6.5-4.5*3)/(47.5-5.5));
-	 System.out.print("\n\t\t\t\tHello");
+	 System.out.print("\n\t\t\t\tHello\t\t\t\t");
  }
 }
