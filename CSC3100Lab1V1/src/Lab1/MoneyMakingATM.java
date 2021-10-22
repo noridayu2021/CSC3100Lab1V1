69a6ed3b1ae6b829bb040e47e2261dc31a00d6d7
7tham <7tham@LAPTOP-2412VUB1>
2021-10-22 13:15:25 +0800

    First Commit For Lab 1 Assignment

---
+++ CSC3100Lab1V1/src/Lab1/MoneyMakingATM.java
@@ -0,0 +1,44 @@
+package Lab1;
+
+public class MoneyMakingATM {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    System.out.println ("---------------------------------------------------------");
+	 	   System.out.println ("/                                                        /");
+	 	  System.out.println ("/                                                        /");
+	 	 System.out.println ("/             Making Money Bank ATM Service              /");
+	 	System.out.println ("/                                                        /");
+	   System.out.println ("/                                                        /");	
+	  System.out.println ("----------------------------------------------------------");
+	  System.out.println ("Welcome!");
+	  System.out.println ("Insert Card");
+	  System.out.println ("Select Language");
+	  System.out.println ("Press 1 for English");
+	  System.out.println ("Press 2 for Malay");
+	  System.out.println ("Enter Pin Number");
+	   System.out.println("---------------------");
+	  System.out.println ("| 1 | 2 | 3 | Clear  |");
+	  System.out.println ("---------------------");
+	  System.out.println ("| 4 | 5 | 6 | Cancel |");
+	  System.out.println ("---------------------");
+	  System.out.println ("| 7 | 8 | 9 | Enter  |");
+	  System.out.println ("----------------------");
+	  System.out.println ("|   | 0 |   |        |");
+	  System.out.println ("----------------------");
+	  System.out.println ("Choose 01 to Check Balance");
+	  System.out.println ("Choose 02 to Withdraw");
+	  System.out.println ("Choose 03 to Deposit");
+	  System.out.println ("Choose 04 to EXIT");
+	  System.out.println ("Operation Selected: 03");
+	  System.out.println ("Enter cash to be deposited: RM 5000.00");
+	  System.out.println ("Your money has been successfully deposited");
+	  System.out.println ("Print Receipt");
+	  System.out.println ("Select | Yes | No |");
+	  System.out.println ("Option Selected: No");
+	  System.out.println (" Ejecting Card");
+	  System.out.println ("Thank you for banking with us");	
+	  System.out.println ("Welcome !");		
+	}
+
+}
