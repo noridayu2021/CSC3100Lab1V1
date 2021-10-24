af1411bbe5500f362a62631f81823084d02f053e
DRAXO99 <92832445+DRAXO99@users.noreply.github.com>
2021-10-25 01:59:42 +0800

    Add files via upload

---
+++ MakingMoneyATM2.java
@@ -0,0 +1,13 @@
+
+public class MakingMoneyATM2 {
+	public static void main(String[] args) {
+		
+		System.out.print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.print("\n@@@ Making Money ATM 2.O @@@");
+		System.out.print("\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		System.out.print("\n\n***WELCOME!***");
+		
+	}
+}
+
+
