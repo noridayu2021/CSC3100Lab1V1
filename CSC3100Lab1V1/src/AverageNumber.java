8680d4098aad80fdfe62125ecee2f81b787e212b
Danish <Danish@PavGamingDanish>
2021-10-27 10:29:10 +0800

    Lab W2

---
+++ CSC3100Lab1V1/src/AverageNumber.java
@@ -0,0 +1,17 @@
+
+public class AverageNumber { //class
+
+	public static void main(String[] args) { //main method
+		// TODO Auto-generated method stub
+		int num1=20; //declare variable and value
+		int num2=30; 
+		int num3=10; 
+		double avg; 
+		
+		avg=(num1+num2+num3)/3; //equation for average
+		System.out.println("Average of three numbers is " + avg); //print average
+	
+
+	}
+
+}
---
+++ CSC3100Lab1V1/src/Weight.java
@@ -0,0 +1,14 @@
+
+public class Weight { //class
+
+	public static void main(String[] args) { //main method
+		// TODO Auto-generated method stub
+		double weight_lbs=5; //declare variable and value
+		double weight_kg;
+		
+		weight_kg=weight_lbs/2.205; //equation
+		
+		System.out.println("Parcel weight in kg is "+weight_kg + "kg"); //print parcel weight in kg
+	}
+
+}
