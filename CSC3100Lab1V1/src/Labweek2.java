3b38f418902b12c086b4a85dbe8db25531635c06
User <User@DESKTOP-R7EFO8P>
2021-10-27 09:58:22 +0800

    Question2 lab week2

---
+++ CSC3100Lab1V1/src/Labweek2.java
@@ -0,0 +1,16 @@
+
+public class Labweek2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ori_price = 500;
+		double discount = 0.5;
+		double price_after_discount = ori_price - (ori_price*discount);
+		System.out.println("The sale price after discount is "  + "RM" + price_after_discount);
+	    
+	
+		
+
+	}
+
+}
