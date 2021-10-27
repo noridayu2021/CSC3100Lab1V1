267b0205cb2150bc5a31df3137a0d01b233c5ed8
User <User@DESKTOP-R7EFO8P>
2021-10-27 10:07:51 +0800

    Q4Labweek2

---
+++ CSC3100Lab1V1/src/Labweek2_Q4.java
@@ -0,0 +1,14 @@
+
+public class Labweek2_Q4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double height,weight,BMI;
+		height = 1.7;
+		weight = 60;
+		BMI = weight/(height*height);
+		System.out.println("The BMI is " + BMI );
+
+	}
+
+}
