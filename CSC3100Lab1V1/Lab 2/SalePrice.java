d53bdc9507d8491a68032db058b08506a98c9f6a
ultim <ultim@DESKTOP-33E1R3S>
2021-10-28 20:54:18 +0800

    Question 2

--- CSC3100Lab1V1/Lab 2/SalePrice.java
+++ CSC3100Lab1V1/Lab 2/SalePrice.java
@@ -1,12 +1,13 @@
-//	Name	:Sahtish Rao Ramuloo
-//	MatricNo:210404
-//	Lab	:2
+//	Name		:Sahtish Rao Ramuloo
+//	MatricNo	:210404
+//	Lab		:2
+//	Description	: To obtain sales price of an object after original price and discount percentage is provided
 public class SalePrice {
 
 	public static void main(String[] args) {
 		
 		int originalprice = 10 ;
-		double discountpercentage = 60;
+		double discountpercentage = 50;
 		double saleprice = originalprice-(originalprice*(discountpercentage/100));
 		
 		System.out.println("Sale price is RM "+saleprice);
