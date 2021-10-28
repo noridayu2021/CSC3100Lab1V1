d6eae20c420f1fb37f4251dc3ea69a63f3550d1a
ultim <ultim@DESKTOP-33E1R3S>
2021-10-28 20:45:47 +0800

    Question 2

---
+++ CSC3100Lab1V1/Lab 2/SalePrice.java
@@ -0,0 +1,15 @@
+//	Name	:Sahtish Rao Ramuloo
+//	MatricNo:210404
+//	Lab	:2
+public class SalePrice {
+
+	public static void main(String[] args) {
+		
+		int originalprice = 10 ;
+		double discountpercentage = 60;
+		double saleprice = originalprice-(originalprice*(discountpercentage/100));
+		
+		System.out.println("Sale price is RM "+saleprice);
+	}
+
+}
