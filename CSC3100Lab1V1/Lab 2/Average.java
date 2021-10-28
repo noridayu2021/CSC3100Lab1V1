f8dab6c83ca92158db6f655e7f37fa6bb527f8c5
ultim <ultim@DESKTOP-33E1R3S>
2021-10-28 20:32:04 +0800

    Question 1

---
+++ CSC3100Lab1V1/Lab 2/Average.java
@@ -0,0 +1,17 @@
+/*	Name		:Sahtish Rao Ramuloo
+ *	Matric No	:210404
+ *	Lab			:2
+ */
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 1;
+		int num2 = 2;
+		int num3 = 3;
+		double average = (num1 +num2 + num3)/3 ;
+		System.out.println("The average of 3 numbers is "+average);
+		
+	}
+
+}
