519b68f8b515ff373be9723978e6ab82d6f17ce7
DANESHWAREN <DANESHWAREN@LAPTOP-GL5OHIF4>
2021-10-29 00:08:36 +0800

    question 4

---
+++ CSC3100Lab1V1/lab 2/WeightOfParcel.java
@@ -0,0 +1,12 @@
+//Name:Daneshwaren Ragupathy
+//Matriks number:209858
+public class WeightOfParcel {
+	public static void main(String[]args) {
+		int weightinpound=100;
+		final double poundtokilo=2.205;
+		double weightinkilo=weightinpound/poundtokilo;//The the formula to calculate the weight in kilo 
+		
+		System.out.println("weight of parcel in kilo is "+weightinkilo);
+	}
+
+}
