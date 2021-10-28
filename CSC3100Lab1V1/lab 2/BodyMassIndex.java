14d2f22ed128207d61086dc55e58379c89c76bf8
DANESHWAREN <DANESHWAREN@LAPTOP-GL5OHIF4>
2021-10-29 00:05:18 +0800

    question 2

---
+++ CSC3100Lab1V1/lab 2/BodyMassIndex.java
@@ -0,0 +1,13 @@
+//Name:Daneshwaren Ragupathy
+//Matriks number:209858
+public class BodyMassIndex {//This is to calculate the body mass index
+	public static void main(String[]args) {
+		int weight=100;
+		double height=1.8;
+		double bodymassindex=(weight/(height*height));//This is the formula to calculate body mass index
+		
+		System.out.println("the body mass index is "+bodymassindex);
+	}
+	
+
+}
