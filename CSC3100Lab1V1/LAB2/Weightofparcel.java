36946a3e0ce0855e64951a171ff02206ba62e268
Bavamithiran <Bavamithiran@DESKTOP-VDJ7QPB>
2021-10-28 22:34:42 +0800

    Question 3

---
+++ CSC3100Lab1V1/LAB2/Weightofparcel.java
@@ -0,0 +1,18 @@
+//NAME 		: BAVAMITHIRAN JEYAPRAGAS
+//MATRIX	: 210468
+//LAB		: 2
+//QUESTION	: The weight of parcel given in pounds and convert into kilograms.
+
+public class Weightofparcel {
+	public static void main(String[]args) {
+		double weightinpound = 100;
+		final double pounttokilo = 2.205;
+		double weightinkilo = weightinpound/pounttokilo;
+		System.out.println("WeightOfParcel in kilogram is" +weightinkilo);
+		
+		
+		
+		
+	}
+
+}
