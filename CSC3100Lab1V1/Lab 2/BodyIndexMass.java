d7d625b605ff82bc0967c8c134142fe8a5978359
ultim <ultim@DESKTOP-33E1R3S>
2021-10-28 21:15:02 +0800

    Question 4

---
+++ CSC3100Lab1V1/Lab 2/BodyIndexMass.java
@@ -0,0 +1,15 @@
+//	Name		:Sahtish Rao Ramuloo
+//	MatricNo	:210404
+//	Lab		:2
+//	Description	: BMI Calculator
+public class BodyIndexMass {
+
+	public static void main(String[] args) {
+		
+		double weight = 55.5; 	//Weight should be in kilograms
+		double height = 1.78; 	//Height should be in metres
+		double BMI = weight/(height*height); 	//Formula to calculate BMI
+		System.out.println("Your Body Mass Index Is "+BMI);
+
+	}
+}
