d5dae8936d76c9cd48ad42dcab1db385390f2251
DANESHWAREN <DANESHWAREN@LAPTOP-GL5OHIF4>
2021-10-28 23:58:14 +0800

    question 1

---
+++ CSC3100Lab1V1/lab 2/Average.java
@@ -0,0 +1,12 @@
+//Name :Daneshwaren Ragupathy
+//Matriks number:209858
+public class Average {
+public static void main(String[]args) {
+	double num1=30;
+	double num2=20;
+	double num3=30;
+	double average=((num1+num2+num3)/3);
+	System.out.println("this is the average "+average);//This is the formula to calculate average
+	
+}
+}
