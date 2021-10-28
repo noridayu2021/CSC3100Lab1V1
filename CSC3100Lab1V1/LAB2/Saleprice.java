1fc46a6e45d05fb8152a09fedb99f99053ff80d6
Bavamithiran <Bavamithiran@DESKTOP-VDJ7QPB>
2021-10-28 22:27:51 +0800

    Question 2

---
+++ CSC3100Lab1V1/LAB2/Saleprice.java
@@ -0,0 +1,16 @@
+//NAME 		: BAVAMITHIRAN JEYAPRAGAS
+//MATRIX	: 210468
+//LAB		: 2
+//QUESTION	: The sale price of an item given an original price and a percentage discount. 
+public class Saleprice {
+	public static void main(String[] args) {
+		int originalprice = 10;
+		double discountpercentage = 50;
+		double saleprice = originalprice -(originalprice*(discountpercentage/100));
+		System.out.println("Sale price is"+saleprice);
+		
+		
+	}
+
+}
+
