01271d1756b2d08db03cf70f33105ccf383fcf48
Bavamithiran <Bavamithiran@DESKTOP-VDJ7QPB>
2021-10-28 22:40:14 +0800

    Question 4

---
+++ CSC3100Lab1V1/LAB2/Bodymassindex.java
@@ -0,0 +1,20 @@
+//NAME 		: BAVAMITHIRAN JEYAPRAGAS
+//MATRIX	: 210468
+//LAB		: 2
+//QUESTION	: Calculate the BMI (BMI=weight (kg)/height2(m)). 
+
+public class Bodymassindex {
+	public static void main(String[]args) {
+		double weight = 70;
+		double height = 1.75;
+		double bmi = weight / (height*height);
+		System.out.println("BodyMassIndex is"+bmi);
+		
+	
+		
+		
+				
+	}
+
+}
+
