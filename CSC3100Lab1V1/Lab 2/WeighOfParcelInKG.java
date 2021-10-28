b64fe0adbf7e52aeb9af32ce9e1b41a76e9d0480
ultim <ultim@DESKTOP-33E1R3S>
2021-10-28 21:09:01 +0800

    Question 3

--- CSC3100Lab1V1/Lab 2/WeighOfParcelInKG.java
+++ CSC3100Lab1V1/Lab 2/WeighOfParcelInKG.java
@@ -7,7 +7,7 @@ public class WeighOfParcelInKG {
 	public static void main(String[] args) {
 		
 	double weightinpound = 10;
-	final double conversionvalue = 2.205; 					// Constant conversion value from pound to kilogram
+	final double conversionvalue = 2.205;	// Constant conversion value from pound to kilogram
 	double weightinkilo = weightinpound/conversionvalue ; 	// Formula to convert pound to kilogram
 	System.out.println("Weight of parcel in kilogram is "+weightinkilo);
 
