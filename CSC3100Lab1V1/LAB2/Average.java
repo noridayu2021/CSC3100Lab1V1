49e2379c578881d52f5817e8f5094696f5a0a1c1
Bavamithiran <Bavamithiran@DESKTOP-VDJ7QPB>
2021-10-28 22:17:15 +0800

    Question 1

---
+++ CSC3100Lab1V1/LAB2/Average.java
@@ -0,0 +1,15 @@
+//NAME 		: BAVAMITHIRAN JEYAPRAGAS
+//MATRIX	: 210468
+//LAB		: 2
+//QUESTION	: The average of three numbers.
+
+public class Average {
+	public static void main(String[] args) {//
+		int num1 = 9;
+		int num2 = 10;
+		int num3 = 11;
+		int sum = num1+num2+num3;
+		double average = (sum/3 );
+		System.out.println("The average of 3 numbers is" + average );
+}
+}
