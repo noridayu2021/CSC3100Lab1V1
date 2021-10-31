b64d97368e4f0327552b1e70b785241acbe712af
User <User@DESKTOP-R7EFO8P>
2021-10-31 15:57:19 +0800

    labweek2

---
+++ CSC3100Lab1V1/src/labweek2Q3.java
@@ -0,0 +1,13 @@
+
+public class labweek2Q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double weight_pound,weight_kg;
+		weight_pound = 15;
+		weight_kg = (weight_pound*0.453592);
+		System.out.println("The weight in kg is  " + weight_kg );
+
+	}
+
+}
---
+++ CSC3100Lab1V1/src/labweek2Q5.java
@@ -0,0 +1,18 @@
+
+public class labweek2Q5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1,num2,num3,num4,num5,num6;
+		num1 = 991;
+		num2 = num1%10;
+		num3 = num1/10;
+		num4 = num3%10;
+		num5 = num3/10;
+		num6 = num2*num4*num5;
+		System.out.println("The multiplier in all digit of 991 is " + num6);
+		
+		
+	}
+
+}
